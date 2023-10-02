398930c69f4edf18e52b0a515b4f67cf7ab8f920
Yongjun OH <132319467+55yong@users.noreply.github.com>
2023-10-03 02:37:08 +0900

    sql 파일 추가
    sql 파일 추가

---
+++ sql/board.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- 생성 시간: 23-10-02 19:28
+-- 서버 버전: 10.4.27-MariaDB
+-- PHP 버전: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `workspace`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `board`
+--
+
+CREATE TABLE `board` (
+  `num` int(11) NOT NULL,
+  `id` varchar(15) NOT NULL,
+  `name` varchar(10) NOT NULL,
+  `subject` varchar(200) NOT NULL,
+  `content` text NOT NULL,
+  `regist_day` varchar(20) NOT NULL,
+  `good` int(11) NOT NULL,
+  `file_name` varchar(40) NOT NULL,
+  `file_type` varchar(40) NOT NULL,
+  `file_copied` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 덤프된 테이블의 인덱스
+--
+
+--
+-- 테이블의 인덱스 `board`
+--
+ALTER TABLE `board`
+  ADD PRIMARY KEY (`num`);
+
+--
+-- 덤프된 테이블의 AUTO_INCREMENT
+--
+
+--
+-- 테이블의 AUTO_INCREMENT `board`
+--
+ALTER TABLE `board`
+  MODIFY `num` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
