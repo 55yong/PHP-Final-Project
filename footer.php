084f284a2acc2dcb0c901005e6e9510bc407d1a2
Yongjun OH <dhdydwns988@gmail.com>
2024-12-04 09:48:55 +0900

    feat: minor change

--- footer.php
+++ footer.php
@@ -2,12 +2,12 @@
     <p id="footer_logo">Pixel Place | <span>카메라 커뮤니티 사이트</span></p>
     <ul id="powered">
         <li>Powered By</li>
-        <li>- Amazon Web Services(https://aws.amazon.com)</li>
+        <li>- 55yong On-premise Server(https://55yong.iptime.org)</li>
         <li>- Apache(https://httpd.apache.org)</li>
     </ul>
     <ul id="author">
         <li>사이트 관리자 연락처</li>
-        <li>- instagram : @55._.yong</li>
+        <li>- instagram : @55yong_</li>
         <li>- email : rmdir@kakao.com</li>
     </ul>
 </div>
